b2bdf54a96a67d15e71502e78569edc5c02409f3
Trenton Haynes <trentonhaynes125@gmail.com>
2025-10-23 20:31:58 -0400

    Updated class.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "splay.h"
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+
+
+
+
+
     return 0;
 }
--- splay.h
+++ splay.h
@@ -3,4 +3,48 @@
 //
 
 #pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Splay {
+
+  public:
+
+    struct Node {
+
+      // Game info
+
+      string title;
+      string release_year;
+      string ign_rating;
+      string genre;
+      vector<string> platforms;
+      string user_rating;
+
+      // Tree info
+
+      Node* left;
+      Node* right;
+
+      Node(string title, string release_year, string ign_rating, string genre, vector<string> platforms, string user_rating);
+
+    };
+
+  private:
+
+    Node* root;
+    bool empty;
+
+  public:
+
+    Splay();
+
+    void deleteSplay(Node* root);
+
+    ~Splay();
+
+  //hello world
+
+  };
 
