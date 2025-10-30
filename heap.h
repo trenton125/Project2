e8d7599e533e09f6ce1e10389a4045d0a2305da8
Alexander Robbins <xanderrobbins10@gmail.com>
2025-10-30 10:21:52 -0400

    test

--- heap.h
+++ heap.h
@@ -1,10 +1,55 @@
-//
-// Created by rgrob on 10/23/2025.
-//
-
 #ifndef HEAP_H
 #define HEAP_H
 
-#endif //HEAP_H
+#include <iostream>
+#include <vector>
+#include <string>
+#include <initializer_list>
+using namespace std;
 
-//lol
+struct Game {
+    string title;
+    int releaseYear;
+    double rating;
+    vector<string> genres;
+    string platforms;
+    string userRating;
+
+    Game() {}
+
+    Game(string t, int y, double r, vector<string> g, string p, string ur) {
+        title = t;
+        releaseYear = y;
+        rating = r;
+        genres = g;
+        platforms = p;
+        userRating = ur;
+    }
+};
+
+class MaxHeap {
+private:
+    vector<Game> heap;
+
+    int parentIndex(int i) const { return (i - 1) / 2; }
+    int leftChild(int i) const { return 2 * i + 1; }
+    int rightChild(int i) const { return 2 * i + 2; }
+
+    void heapifyUp(int i);
+    void heapifyDown(int i);
+
+public:
+    MaxHeap() {}
+
+    void insertHeap(const string &title, int year, double rating,
+                    const vector<string> &genres, const string &platforms, const string &userRating);
+
+    bool search(const string &title) const;
+    void display() const;
+    Game getMax() const;
+
+    int size() const { return heap.size(); }
+    bool empty() const { return heap.empty(); }
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "splay.h"
+#include "heap.h"
 #include <string>
 using namespace std;
 // #include "Bridges.h"
@@ -9,10 +10,23 @@ using namespace std;
 // using namespace bridges;
 
 int main() {
+    MaxHeap heap;
 
-    Splay splay;
-    splay.insertSplay("HollowKnight: Silksong", 10, {"souls-like", "metroidvania"}, "PC", "10");
-    splay.splayPrintInorder();
 
-    return 0;
+
+    heap.insertHeap("Zelda: Breath of the Wild", 2017, 9.8, {"Adventure", "Open World"}, "Switch", "9.7");
+    heap.insertHeap("Halo: Combat Evolved", 2001, 9.0, {"Shooter", "Sci-Fi"}, "Xbox", "9.1");
+    heap.insertHeap("Animal Crossing: New Horizons", 2020, 8.5, {"Simulation"}, "Switch", "8.9");
+    heap.insertHeap("Mario Kart 8 Deluxe", 2017, 9.3, {"Racing", "Multiplayer"}, "Switch", "9.5");
+    heap.insertHeap("God of War", 2018, 9.6, {"Action", "Adventure"}, "PlayStation", "9.8");
+
+
+    heap.display();
+    heap.insertHeap("Zzzz Ultimate Game", 2025, 10.0, {"Fantasy", "RPG"}, "PC", "10.0");
+
+    heap.getMax();
+
+    heap.display();
+
 }
+
