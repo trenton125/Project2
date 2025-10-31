9914c419c647a9b4807728a972605bf3aedeea28
M0XY <moxymirage@gmail.com>
2025-10-30 21:45:12 -0400

    Remove build folder from git

--- cmake-build-debug/CMakeFiles/fc-tmp/sfml/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing download step for sfml")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/M0XY/Project2/cmake-build-debug/CMakeFiles/fc-tmp/sfml/sfml-gitclone.cmake")
-
-endblock()
